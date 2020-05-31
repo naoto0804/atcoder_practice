8b97deb842e7a0ff3d7e275c463ebb6ed9411349
Naoto Inoue <kingfisher0804@gmail.com>
2020-05-31 22:49:45 +0900

    abc169

---
+++ abc/169/d.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+/**
+ * For DEBUG
+ * https://github.com/ta7uw/cpp-pyprint
+ */
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+map<ll, ll> prime_factorize(ll N) {
+    map<ll, ll> res;
+    for (ll i = 2; i * i <= N; ++i) {
+        while (N % i == 0) {
+            res[i]++;
+            N /= i;
+        }
+    }
+    if (N != 1) res[N] = 1;
+    return res;
+}
+
+void Main() {
+    ll N; cin >> N;
+    map<ll, ll> x = prime_factorize(N);
+    // 2^{1}, 2^{2}, .. , 2^{N}と割り切って行って限界を見つける
+    ll cnt_total = 0;
+    for (auto a: x){
+        ll tmp = a.second;
+        rep2(i, 1, 100){ // 2^{40}で10^{12}なのでかなり余裕があるはず
+            if (tmp >= i){
+                tmp -= i;
+            } else {
+                cnt_total += (i - 1);
+                break;
+            }
+        }
+    }
+    cout << cnt_total << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
