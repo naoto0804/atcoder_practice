d3fa5eb9e3902a5a00d46950b4a482dddae93f41
Naoto Inoue <kingfisher0804@gmail.com>
2020-06-22 20:03:55 +0900

    better solution using prebuilt ^ operator

--- abc/171/e.cpp
+++ abc/171/e.cpp
@@ -25,41 +25,12 @@ const ll INF = 1000000000000000000L;
 #endif
 
 void Main() {
-    // (i) 各桁ごとに独立 (ii) 各桁の和のmod2が0なら反転せず，1なら反転
-    // xor演算子を@としてa1@a2@...@aNはN-1回繰り返された(b1@b2@..@bN)と同じ，Nが偶数なのでa1@a2@...@aN=b1@b2@..@bN
-    // 更にこの値をSとしてS+ai=(b1@b1)@..@(bi)@..@(bN@bN)=bi
-    ll N; cin >> N;
-    ll MAX = 30; // 限界の大きさまでカバー
-    ll bin[N][MAX];
-    ll ans[N][MAX];
-
-    // 値の上限を大まかに評価した上で二進数変換
-    rep(i, N){
-        ll a;
-        cin >> a;
-        rrep(j, MAX - 1, 0){
-            bin[i][j] = a % 2;
-            a = a / 2;
-        }
+    ll N; cin >> N; vector<ll> a(N); rep(i, N){cin >> a.at(i);}
+    ll S = a.at(0);
+    rep(i, N - 1){
+        S = (S ^ a.at(i + 1)); // llで表記していてもbit演算は普通に出来る
     }
-    rep(j, MAX){
-        ll tmp = 0;
-        rep(i, N){tmp += bin[i][j];}
-        if (tmp % 2 == 0){
-            rep(i, N){ans[i][j] = bin[i][j];}
-        } else {
-            rep(i, N){ans[i][j] = (bin[i][j] == 0) ? 1 : 0;}
-        }
-    }
-
-    // 二進数to十進数
-    rep(i, N){
-        ll tmp = 0;
-        rep(j, MAX){
-            tmp = tmp * 2 + ans[i][j];
-        }
-        cout << tmp << " ";
-    }
+    rep(i, N){cout << (S ^ a.at(i)) << " ";}
 }
 
 int main() {
