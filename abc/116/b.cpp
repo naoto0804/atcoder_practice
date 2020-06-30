78eb4c82436ac26a86ea32734e9553b1e1e4e35e
Naoto Inoue <kingfisher0804@gmail.com>
2020-07-01 01:10:05 +0900

    daily

---
+++ abc/116/b.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+/**
+ * For DEBUG
+ * https://github.com/ta7uw/cpp-pyprint
+ */
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    ll s; cin >> s;
+    ll cnt = 1;
+    ll prev = s;
+    set<ll> x; x.insert(prev);
+    while (true){
+        if (prev % 2 == 0){
+            prev = prev / 2;
+        } else {
+            prev = 3 * prev + 1;
+        }
+        cnt++;
+        if (x.find(prev) != x.end()){break;}
+        x.insert(prev);
+    }
+    cout << cnt << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
