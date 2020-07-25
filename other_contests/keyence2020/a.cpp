c4b6cf9af890d7e3c5135470dadf26366ed1d720
Naoto Inoue <kingfisher0804@gmail.com>
2020-07-26 01:41:27 +0900

    practice

---
+++ other_contests/keyence2020/a.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+/**
+ * For DEBUG
+ * https://github.com/ta7uw/cpp-pyprint
+ */
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    ll H, W, N; cin >> H >> W >> N;
+    ll cnt = INF;
+    rep2(h, 0, H + 1){rep2(w, 0, W + 1){
+        ll tmp_area = H * W - (H - h) * (W - w);
+        ll tmp_cnt = h + w;
+        if (tmp_area >= N){
+            if (tmp_cnt < cnt){cnt = tmp_cnt;}
+        }
+    }}
+    cout << cnt << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
