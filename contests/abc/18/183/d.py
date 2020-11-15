89a81b6a198b84f9fce8e8366c612c3bd40cee81
Naoto Inoue <kingfisher0804@gmail.com>
2020-11-15 23:24:55 +0900

    abc183

---
+++ contests/abc/18/183/d.py
@@ -0,0 +1,58 @@
+# # Make IO faster
+# import sys
+# input = sys.stdin.readline
+
+# # get single (or) multiple str
+# X = input()
+
+# # get single int
+# N = int(input())
+# # get multiple int (e.g., 2)
+# X, Y = map(int, input().split())
+# # get multiple int (e.g., 2) for N lines
+# XY = [list(map(int, input().split())) for _ in range(N)]
+
+# from IPython import embed; embed(); exit();
+
+# 全部入り
+import sys, re
+from collections import deque, defaultdict, Counter
+from math import ceil, sqrt, hypot, factorial, pi, sin, cos, radians, fabs, gcd
+from itertools import accumulate, permutations, combinations, product
+from operator import itemgetter, mul
+from copy import deepcopy
+from string import ascii_lowercase, ascii_uppercase, digits
+from bisect import bisect, bisect_left, insort_left
+# from fractions import gcd
+from heapq import heappush, heappop
+from functools import reduce
+import numpy as np
+def input(): return sys.stdin.readline().strip()
+def INT(): return int(input())
+def MAP(): return map(int, input().split())
+def LIST(): return list(map(int, input().split()))
+def ZIP(n): return zip(*(MAP() for _ in range(n)))
+sys.setrecursionlimit(10 ** 9)
+INF = float('inf')
+mod = 10 ** 9 + 7
+
+N, W = list(MAP())
+MAX_TIME = 200001
+# 増減を記録 O(N)
+diff = [0 for _ in range(MAX_TIME)]
+for i in range(N):
+    S, T, P = list(MAP())
+    diff[S] += P
+    diff[T] -= P
+
+#  各時刻の値を確認O(MAX_TIME)
+acc = 0
+for t in range(MAX_TIME):
+    acc += diff[t]
+    # if diff[t] != 0:
+    #     print(t, acc)
+    if acc > W:
+        print("No")
+        exit()
+
+print("Yes")
--- main.cpp
+++ main.cpp
@@ -11,10 +11,10 @@ using Graph = vector<vector<ll> >;
 #define rep(i, n) for(ll i=0;i<(ll)(n);i++)
 #define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
 #define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
-const int dx[4] = {1, 0, -1, 0};
-const int dy[4] = {0, 1, 0, -1};
-const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
-const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const int dx[3] = {-1, 0, -1};
+const int dy[3] = {0, -1, -1};
+// const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+// const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
 const ll MOD = 998244353;
 const ll INF = 1000000000000000000L;
 #ifdef __DEBUG
@@ -26,13 +26,6 @@ const ll INF = 1000000000000000000L;
 #include "cpp-pyprint/pyprint.h"
 
 #endif
-
-bool op(bool cur, bool is_lamp, bool is_block){
-    bool ans = cur;
-    if (is_lamp){ans = true;}
-    if (is_block){ans = false;}
-    return ans;
-}
 
 void Main() {
 }
